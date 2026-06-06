5ac8ebe425961b9c357aa349e5a737745fdb669c
mjaabdullah <mdjuelahmedabdullah@gmail.com>
2026-06-06 21:44:34 +0600

    Add search function

--- src/app/all-facilities/page.jsx
+++ src/app/all-facilities/page.jsx
@@ -1,7 +1,7 @@
 import FacilityCard from "@/components/facilities/FacilityCard";
 import Filter from "@/components/facilities/Filter";
+import Search from "@/components/facilities/Search";
 import { getAllFacilities, getFacilityCategories } from "@/lib/data";
-import { SearchField } from "@heroui/react";
 
 const AllFacilitiesPage = async ({ searchParams }) => {
   const { category, search } = await searchParams;
@@ -12,17 +12,7 @@ const AllFacilitiesPage = async ({ searchParams }) => {
     <div className="container mx-auto px-4 py-10">
       <h1 className="text-4xl font-bold">All Facilities</h1>
       <div className="my-5 flex justify-between gap-6">
-        <SearchField name="search">
-          <SearchField.Group>
-            <SearchField.SearchIcon />
-            <SearchField.Input
-              className="w-[280px]"
-              placeholder="Search facilities"
-            />
-            <SearchField.ClearButton />
-          </SearchField.Group>
-        </SearchField>
-
+        <Search />
         <Filter categories={categories} />
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 justify-items-center mt-5">
---
+++ src/components/facilities/Search.jsx
@@ -0,0 +1,40 @@
+"use client";
+import { Button, SearchField } from "@heroui/react";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useState } from "react";
+
+const Search = () => {
+  const router = useRouter();
+  const [search, setSearch] = useState("");
+  const searchParams = useSearchParams();
+  const handleSearch = () => {
+    const params = new URLSearchParams(searchParams.toString());
+    if (!search) {
+      params.delete("search");
+    } else {
+      params.set("search", search);
+    }
+    router.push(`?${params.toString().toLowerCase()}`);
+  };
+  return (
+    <div className="flex items-center gap-2">
+      {" "}
+      <SearchField name="search" onChange={(value) => setSearch(value)}>
+        <SearchField.Group>
+          <SearchField.SearchIcon />
+          <SearchField.Input className="w-70" placeholder="Search facilities" />
+          <SearchField.ClearButton />
+        </SearchField.Group>
+      </SearchField>
+      <Button
+        onClick={handleSearch}
+        variant="secondary"
+        className={`bg-green-500 text-white rounded-md`}
+      >
+        Search
+      </Button>
+    </div>
+  );
+};
+
+export default Search;
